377ca2bafc72c2ee0ae17fdedfc7a6655509716c
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-07 13:57:31 +0900

    Create UnionFind.swift

---
+++ UnionFind.swift
@@ -0,0 +1,25 @@
+struct UnionFind {
+    var parent: [Int]
+
+    init(cnt: Int) { parent = Array(0...cnt) }
+
+    mutating func root(of x: Int) -> Int {
+        if parent[x] == x { return x }
+        parent[x] = root(of: parent[x])
+        return parent[x]
+    }
+
+    mutating func merge(_ x: Int, with y: Int) {
+        let (x, y) = (root(of: x), root(of: y))
+        if x == y { return }
+        parent[x] = y
+    }
+
+    mutating func isUnion(_ x: Int, _ y: Int) -> Bool {
+        root(of: x) == root(of: y)
+    }
+}
+
+
+// 자료구조? 는 애매하지만?
+// 이렇게 쓰는게 편하니까 그냥 하는거지 뭐
