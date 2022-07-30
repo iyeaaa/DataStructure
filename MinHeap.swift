921ae43707e83b970f1e30d324936102dc9bee57
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2022-07-30 22:40:38 +0900

    Update MinHeap.swift

--- MinHeap.swift
+++ MinHeap.swift
@@ -1,69 +1,64 @@
+
 struct Heap<T: Comparable> {
     var heap = [T]()
 
-    init() {}
-    init(_ h: T) { heap.append(h); heap.append(h) }
+    init() { }
+    init(_ data: T) { heap.append(data); heap.append(data) }
 
-    mutating func insert(_ h: T) {
-        heap.append(h)
-        if heap.count == 1 { heap.append(h); return }
+    var isEmpty: Bool { heap.count <= 1 }
 
-        func isMoveUp(_ insertIdx: Int) -> Bool {
-            if insertIdx <= 1 { return false }
-            return heap[insertIdx] < heap[insertIdx/2]
-        }
+    var first: T? { isEmpty ? nil : heap[1] }
 
-        var insertIdx = heap.count - 1
-        while isMoveUp(insertIdx) {
-            let prntIdx = insertIdx / 2
-            heap.swapAt(insertIdx, prntIdx)
-            insertIdx = prntIdx
+    mutating func insert(_ data: T) {
+        heap.append(data)
+        if isEmpty { heap.append(data)}
+
+        var isrtIdx = heap.count - 1
+        while isrtIdx >= 1 && heap[isrtIdx/2] > heap[isrtIdx] {
+            heap.swapAt(isrtIdx, isrtIdx/2)
+            isrtIdx = isrtIdx/2
         }
     }
 
-    enum moveDownStatus { case none, lf, ryt }
+    enum Direction { case none, lf, ryt }
     mutating func pop() -> T? {
-        if heap.count <= 1 { return nil }
+        if isEmpty { return nil }
 
         heap.swapAt(1, heap.count-1)
         let returnData = heap.popLast()!
 
-        func moveDown(_ poppedIdx: Int) -> moveDownStatus {
-            let lfChlIdx = poppedIdx * 2
-            let rytChlIdx = lfChlIdx + 1
-            if lfChlIdx >= heap.count { // 왼쪽 자손 없음
+        func moveDown(_ pop: Int) -> Direction {
+            let lf = pop * 2
+            let ryt = lf + 1
+            if lf >= heap.count {
                 return .none
             }
-            if rytChlIdx >= heap.count { // 왼쪽 자손 있지만 오른쪽 자손 없음
-                return heap[lfChlIdx] < heap[poppedIdx] ? .lf : .none
+            if ryt >= heap.count {
+                return heap[lf] < heap[pop] ? .lf : .none
             }
-            if heap[lfChlIdx] >= heap[poppedIdx] && heap[rytChlIdx] >= heap[poppedIdx] {
-                return .none // 왼.오 모두 있으나 자손이 더 큼 -> 바꿀필요 없음
+            if heap[lf] >= heap[pop] && heap[ryt] >= heap[pop] {
+                return .none
             }
-            if heap[lfChlIdx] < heap[poppedIdx] && heap[rytChlIdx] < heap[poppedIdx] {
-                return heap[lfChlIdx] < heap[rytChlIdx] ? .lf : .ryt
-                // 왼.오 모두 가능하기에 더 작은것 선택
+            if heap[lf] < heap[pop] && heap[ryt] < heap[pop] {
+                return heap[lf] < heap[ryt] ? .lf : .ryt
             }
-            // 왼.오 중 하나가 가능함 -> 가능한것 찾아서 리턴
-            return heap[lfChlIdx] < heap[poppedIdx] ? .lf : .ryt
+            return heap[lf] < heap[pop] ? .lf : .ryt
         }
 
-        var poppedIdx = 1
+        var pop = 1
         while true {
-            switch moveDown(poppedIdx) {
+            switch moveDown(pop) {
             case .none:
                 return returnData
             case .lf:
-                let lfChlIdx = poppedIdx*2
-                heap.swapAt(poppedIdx, lfChlIdx)
-                poppedIdx = lfChlIdx
+                let chl = pop * 2
+                heap.swapAt(pop, chl)
+                pop = chl
             case .ryt:
-                let rytChlIdx = poppedIdx*2 + 1
-                heap.swapAt(poppedIdx, rytChlIdx)
-                poppedIdx = rytChlIdx
+                let chl = pop*2 + 1
+                heap.swapAt(pop, chl)
+                pop = chl
             }
         }
     }
 }
-
-개발자 소들이의 소스코드에서 약간의 오류수정, 간결화, 최적화한 코드
